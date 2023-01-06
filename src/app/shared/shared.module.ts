0e469c2781dd9cba66c8219435ce8e6c91abc771
Nelson Sambula <nelsinho1031@gmail.com>
2023-01-06 15:03:10 -0600

    Creating menu bar

---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,11 @@
+import { PrimeNgModule } from './../prime-ng/prime-ng.module';
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MenuComponent } from './menu/menu.component';
+
+@NgModule({
+  declarations: [MenuComponent],
+  imports: [CommonModule, PrimeNgModule],
+  exports: [MenuComponent],
+})
+export class SharedModule {}
